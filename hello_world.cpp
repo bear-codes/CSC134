413df5a99141a60832a450201fa2b09c60a8166a
Bear Codes <lwlynch515@gmail.com>
2024-08-21 19:56:17 -0400

    A little message

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+//@author: Levi Lynch
+//@date: 8/21/24
+//@purpose: This is my first C++ program and it simply prints the srting hello world! to the console
+
+//include the iostream directive
+#include <iostream>
+
+//define the main() function/method
+int main() 
+{
+    //use the std output for the console and print the hello world! string and create a new line using endl;
+    std::cout<<"hello world!"<<std::endl; 
+
+}
